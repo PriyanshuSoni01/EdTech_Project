bd842cae620aed1ac55a7b3261d149731e5339a4
Priyanshu <sonipriyanshu953@gmail.com>
2025-08-15 14:29:26 +0530

    Folder Structure improvement

--- Backend/index.js
+++ Backend/index.js
@@ -22,8 +22,9 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 })
 
 const app = express()
-app.listen(4000, ()=>{
-    console.log('Server is running on port 3000!!!')
+const port = process.env.PORT || 4000
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}!!!`)
 })
 
 // Middlewares
